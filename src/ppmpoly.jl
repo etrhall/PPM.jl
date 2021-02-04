6c37dfcc5794e1f36b5dfdcc019a3448f98cf697
etrhall <edward.hall@qmul.ac.uk>
2021-02-04 16:22:58 +0000

    add method for individual train/predict control

--- src/ppmpoly.jl
+++ src/ppmpoly.jl
@@ -70,3 +70,80 @@ function modelpoly!(
     # return [resultdf(r) for r in results]
     return results
 end
+
+
+# New method to toggle prediction and training for individual sequences
+function modelpoly!(
+    ppm::AbstractPPM,
+    seqs::Vector{Sequence};
+    times::Vector{Vector{Float64}} = [Float64[]],
+    train::Vector{Bool} = Bool[],
+    predict::Vector{Bool} = Bool[],
+    returndistribution::Bool = true,
+    returnentropy::Bool = true
+)
+    nn = length(seqs)
+    ns = map(length, seqs)
+
+    if map(y -> any(diff(y) .< 0), times) |> any
+        error("decreasing values of time are not permitted")
+    end
+
+    if length(train) != nn
+        error("train must be same length as number of sequences")
+    end
+
+    if length(predict) != nn
+        error("predict must be same length as number of sequences")
+    end
+
+    if all(isempty.(times))
+        times = [collect(1.0:n) for n in ns]
+    end
+
+    alltimes = vcat(times...) |> sort |> unique
+
+    results = [
+        SequencePrediction(returndistribution, returnentropy, ppm.decay)
+        for i in 1:nn
+    ]
+
+    for t in alltimes
+        timeevents = map(x->findfirst(isequal(t), x), times)
+
+        # Predict
+        for (i, e) in enumerate(timeevents)
+            if isnothing(e); continue end
+            x = seqs[i]
+            j = findfirst(isequal(t), times[i])
+
+            if predict[i]
+                context = if j == 1 || ppm.orderbound < 1
+                    Sequence()
+                else
+                    subseq(x, max(1, j - ppm.orderbound), j - 1)
+                end
+                insertprediction!(results[i], predictsymbol(ppm, x[j], context, j, t))
+            end
+        end
+
+        # Train
+        for (i, e) in enumerate(timeevents)
+            if isnothing(e); continue end
+            x = seqs[i]
+            j = findfirst(isequal(t), times[i])
+
+            if train[i]
+                if ppm.decay; push!(ppm.alltime, t) end
+                fullonly = false
+                for h in max(1, j - ppm.orderbound):j
+                    fullonly = ppminsert!(ppm, subseq(x, h, j), j, t, fullonly)
+                end
+                ppm.numobservations += 1
+            end
+        end
+    end
+
+    # return [resultdf(r) for r in results]
+    return results
+end
